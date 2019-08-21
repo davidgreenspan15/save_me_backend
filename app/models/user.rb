bcf64871331b9f78214d34a2dcb1949d9ce0b565
davidgreenspan15 <ramigreenspan@gmail.com>
2019-08-21 15:10:34 -0400

    edited user controls

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :transactions
-  has_many :categories, through: :transactions
+  has_many :transactions, :dependent => :delete_all
+  has_many :categories, through: :transactions, :dependent => :delete_all
   validates :name, :username, :stock_level, presence: true
   has_secure_password
 end
